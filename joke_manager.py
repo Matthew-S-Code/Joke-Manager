b2f0a297220b8604ef11dde09b695d3947f12d29
Matthew-S-Code <mattsal.home@gmail.com>
2026-09-04 19:02:40 +0100

    removed pseudocode and added try except for the api

--- joke_manager.py
+++ joke_manager.py
@@ -1,20 +1,5 @@
 import urllib.request, json, os
 
-"""
-create a class that initially defines itself and makes sure that itself and new files stay in 
-the local folder and create a file called saved_jokes.txt and save it to self.filepath
-
-create a subroutine for fetching jokes
-this goes to the api url, requests a joke and the joke is acquired as the response
-read the response, decode it and assign it to a new variable after turning it into a dictionary
-check if setup and punchline are in there, create a new variable combining the setup and punchline 
-and then return the setup and punchline combo
-
-create a subroutine to save the joke
-this takes in the parameter of the joke text
-open the self.filepath in append mode and write in the joke text using the 'both' variable to make it easier
-
-"""
 
 class JokeManager:
     def __init__(self):
@@ -23,14 +8,17 @@ class JokeManager:
         self.filepath = os.path.join(current_folder, "saved_jokes.txt")
 
     def fetch_joke(self):
-        with urllib.request.urlopen(self.api_url) as response:
-            raw_string = response.read().decode("utf-8")
-            dict_of_joke = json.loads(raw_string)
-            if "setup" in dict_of_joke and "punchline" in dict_of_joke:
-                setup = dict_of_joke["setup"]
-                punchline = dict_of_joke["punchline"]
-                both = f"{setup}\n{punchline}\n\n"
-                return both
+        try:
+            with urllib.request.urlopen(self.api_url) as response:
+                raw_string = response.read().decode("utf-8")
+                dict_of_joke = json.loads(raw_string)
+                if "setup" in dict_of_joke and "punchline" in dict_of_joke:
+                    setup = dict_of_joke["setup"]
+                    punchline = dict_of_joke["punchline"]
+                    both = f"{setup}\n{punchline}\n\n"
+                    return both
+        except Exception as e:
+            return f"Error fetching joke {e}"
 
     def save_joke(self, joke_text):
         with open(self.filepath, "a") as my_jokes:
